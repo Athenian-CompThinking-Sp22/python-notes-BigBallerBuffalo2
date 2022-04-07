4321eb8ffee9e3d7616736020c0056affdc6ec09
darren <25drosenberg@athenian.org>
2022-04-07 12:30:12 -0700

    d period day one, done

--- src/print_input.py
+++ src/print_input.py
@@ -6,3 +6,17 @@
 #############################
 
 # Write code here:
+name = 'darren'
+print(f'hello {name}')
+
+name = input('Please enter your name: ')
+print(f'wow you have a bad name')
+print(':/...     disappointing')
+name = input('knock knock ')
+print(f'no one')
+
+print('lol')
+
+num1 = int(input('Enter a number: '))
+num2 = int(input('enter another number: '))
+print(num1 + num2)
